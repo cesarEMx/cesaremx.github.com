0ce2e0787df9c00bc2cccea73fab8be248a22df2
cesarEMx <cesaresquivelao2105@gmail.com>
2023-04-21 16:23:20 -0600

    Agregamos proyecto boletines
    no tiene js

---
+++ boletines/scripts/main.js
@@ -0,0 +1,19 @@
+
+/*var contenedor = document.querySelectorAll("a");
+contenedor.forEach(function(contedio){
+    console.log(contedio);
+})*/
+
+/*
+let cerrar = document.querySelectorAll("x");
+cerrar.forEach(function(cerra){
+    cerra.addEventListener("click",function(){
+        console.log(":3");
+    });
+});*/
+/**let icono = document.querySelectorAll("i");
+icono.forEach(function(ico){
+    ico.classList.remove("");
+    ico.classList.add("fa-star")
+}); */
+lef contenedor = document.querySelector(".contenido")
